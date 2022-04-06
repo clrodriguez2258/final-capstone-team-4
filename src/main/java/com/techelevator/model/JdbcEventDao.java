f9348b96028b3765f0ab0daac4d8c38249443ff8
Angel Martinez Barrientos <angelmartinezbarrientos@gmail.com>
2022-04-06 12:54:07 -0400

    final capstone progress

---
+++ src/main/java/com/techelevator/model/JdbcEventDao.java
@@ -0,0 +1,31 @@
+package com.techelevator.model;
+
+import com.techelevator.authentication.PasswordHasher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class JdbcEventDao implements EventDao{
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    public JdbcEventDao(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Event saveEvent(String eventName, java.sql.Date  eventDate, java.sql.Time eventTime) {
+
+        Event newEvent = new Event();
+         Long newId =  jdbcTemplate.queryForObject("INSERT INTO event (event_name, event_date, event_time) values (?,?,?) RETURNING event_id", Long.class,
+                                        eventName,eventDate,eventTime);
+         newEvent.setEventId(newId);
+         newEvent.setEventName(eventName);
+         newEvent.setEventDate(eventDate);
+         newEvent.setEventTime(eventTime);
+
+        return newEvent;
+    }
+}
