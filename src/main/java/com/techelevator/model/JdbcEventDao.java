69ceb266ef89c2cb393de9fbe1a3c94ad836898e
Christian Rodriguez <clrodriguez.210@gmail.com>
2022-04-11 11:39:46 -0500

    04.10

--- src/main/java/com/techelevator/model/JdbcEventDao.java
+++ src/main/java/com/techelevator/model/JdbcEventDao.java
@@ -3,47 +3,85 @@ package com.techelevator.model;
 import com.techelevator.authentication.PasswordHasher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
-public class JdbcEventDao implements EventDao{
+public class JdbcEventDao implements EventDao {
     private JdbcTemplate jdbcTemplate;
+
     @Autowired
     public JdbcEventDao(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     @Override
-    public Event createNewEvent(String eventName, LocalDate  eventDate, LocalTime eventTime, LocalDate decisionDate) {
+    public Event createNewEvent(String eventName, LocalDate eventDate, LocalTime eventTime, LocalDate decisionDate) {
 
         Event newEvent = new Event();
-         Long newId =  jdbcTemplate.queryForObject("INSERT INTO events (event_name, event_date, event_time,decision_date) values (?,?,?,?) RETURNING event_id", Long.class,
-                                        eventName,eventDate,eventTime, decisionDate);
-         newEvent.setEventId(newId);
-         newEvent.setEventName(eventName);
-         newEvent.setEventDate(eventDate);
-         newEvent.setEventTime(eventTime);
-         newEvent.setDecisionDate(decisionDate);
+        Long newId = jdbcTemplate.queryForObject("INSERT INTO events (event_name, event_date, event_time,decision_date) values (?,?,?,?) RETURNING event_id", Long.class,
+                eventName, eventDate, eventTime, decisionDate);
+        newEvent.setEventId(newId);
+        newEvent.setEventName(eventName);
+        newEvent.setEventDate(eventDate);
+        newEvent.setEventTime(eventTime);
+        newEvent.setDecisionDate(decisionDate);
 
         return newEvent;
     }
+
+
+    @Override
+    public List<Event> getEventByGuestId(Long guestId) {
+        List<Event> eventByGuest = new ArrayList<>();
+
+        String sql = "SELECT * " +
+                "FROM guest " +
+                "LEFT JOIN guest_event ON guest.guest_id = guest_event.guest_id " +
+                "WHERE guest_id = ?";
+
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, guestId);
+        while (results.next()) {
+            eventByGuest.add(mapRowToEvent(results));
+        }
+
+        return eventByGuest;
+
+    }
+
+    public List<Event> getEventByEventId(Long guestId) {
+        List<Event> eventByEventId = new ArrayList<>();
+
+        String sql = "SELECT * " +
+                "FROM events " +
+                "WHERE event_id = ?";
+
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, guestId);
+        while (results.next()) {
+            eventByEventId.add(mapRowToEvent(results));
+        }
+
+        return eventByEventId;
+
+    }
+
+
+
+    private Event mapRowToEvent(SqlRowSet results) {
+
+        Event event = new Event();
+        event.setEventId(results.getLong("event_id"));
+        event.setRestaurantId(results.getLong("restaurant_id"));
+        event.setEventName(results.getString("event_name"));
+        event.setEventDate(results.getDate("event_date").toLocalDate());
+        event.setEventTime(results.getTime("event_time").toLocalTime());
+        event.setDecisionDate(results.getDate("decision_date").toLocalDate());
+    return event;
+    }
 }
-//   public User saveUser(String userName, String password, String role) {
-//        byte[] salt = passwordHasher.generateRandomSalt();
-//        String hashedPassword = passwordHasher.computeHash(password, salt);
-//        String saltString = new String(Base64.encode(salt));
-//        long newId = jdbcTemplate.queryForObject(
-//                "INSERT INTO app_user(user_name, password, salt, role) VALUES (?, ?, ?, ?) RETURNING id", Long.class,
-//                userName, hashedPassword, saltString, role);
-//
-//        User newUser = new User();
-//        newUser.setId(newId);
-//        newUser.setUsername(userName);
-//        newUser.setRole(role);
-//
-//        return newUser;
-//    }
